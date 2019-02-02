eeb92a2e60e43dc5cd0065b6f1e55728c3447f2e
Alec Kerrigan <imor231@gmail.com>
2019-02-02 12:25:52 -0500

    Added welcome functionality

--- source/knightroko.py
+++ source/knightroko.py
@@ -9,6 +9,7 @@ import re
 import sys
 import signal
 import discord
+import asyncio
 from plugins import COMMANDS, INLINES, FILTERED_CHANNELS
 
 TOKEN_FILE = "data/discord_token.txt"
@@ -55,9 +56,12 @@ async def on_message(message):
 
 @client.event
 async def on_member_join(member):
-	print("Welcome to the UCF Anime Spot Discord " + member.name + "!")
-	print("Please state your first name, as well if you are a dues paying memeber")
-	print("Then, you will be given a role and entry into the rest of the server!")
+	channel = client.get_channel(535609411540877339)
+	message = "Welcome to the UCF Anime Spot Discord " + member.mention + \
+	"! Please state your first name, as well as if you are a dues paying member." + \
+	"Then, you will be given a role and entry into the rest of the server!"
+	await asyncio.sleep(2)
+	await channel.send(message)
 
 
 def load_token():
